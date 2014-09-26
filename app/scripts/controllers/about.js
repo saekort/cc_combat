b0379d96a72f2a402e806c6f49a6419a3969093b
Simon Kort <simon.kort@gmail.com>
2014-09-26 09:25:36 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ccCombatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ccCombatApp
+ */
+angular.module('ccCombatApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
