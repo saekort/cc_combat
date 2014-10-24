ae9bda85452e0b215d1cc4c1036ac92030218562
Simon Kort <simon.kort@gmail.com>
2014-10-24 12:27:20 +0200

    Massive updates. Most of the tracker works functionally.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 (function() {
 	
-function MainCtrl() {
+function MainCtrl($location) {
 	// Important: Only do presentational logic in controllers
 	// Important: Do business logic in services
 	
@@ -10,29 +10,62 @@ function MainCtrl() {
 	this.combat = [];
 	this.newInitiative = {};
 	
-	// Demo initiative orders
-	this.combat = [
-	           	  new Initiative('Ragnor the Red',11,'pc'),
-	           	  new Initiative('Violet the Vile',16,'pc'),
-	           	  new Initiative('Nightstalker Nydia',4,'pc'),
-	           	  new Initiative('Peter Patronum',26,'pc'),
-	           	  new Initiative('Neruban Ninja',3,'pc')
-	           	  ];
+	// See if we have to run the demo
+	if($location.search().demo)
+	{
+		this.demo = true;
+	}
+	else
+	{
+		this.demo = false;
+	}
+	
+	if(this.demo)
+	{
+		// Demo initiative orders
+		this.combat = [
+		           	  new Initiative('Ragnor the Red',11,'pc'),
+		           	  new Initiative('Violet the Vile',16,'pc'),
+		           	  new Initiative('Nightstalker Nydia',4,'pc'),
+		           	  new Initiative('Peter Patronum',26,'pc'),
+		           	  new Initiative('Neruban Ninja',3,'pc'),
+		           	  new Initiative('Barry the Bugbear', 14, 'monster')
+		           	  ];
+	}
 	
 	// Default sort order (on initiative)
 	this.sort = '-init';
 	
 	// Combat mode false = 'input' or true = 'combat'
 	this.mode = 'input';
+	
+	this.quotes = ['Roll for initiative monkey boy!',
+	               'Just theoretically, how does one unsummon a demon?',
+	               'What a useless scroll. It just says, HASTUR HASTUR HASTUR over and over again...',
+	               'He hit me for HOW MUCH?????',
+	               'AAAAAAAAAAAAAAAAAAAAAAAAHHHHHHH!',
+	               'Don\'t worry, he is probably just first level.',
+	               'I have this dungeon at home, I know where everything is!',
+	               'I see HOW MANY wights?!',
+	               'A Nightmare, huh? I\'ll attack for one round and prepare to run.',
+	               'They need a twenty to hit me, I\'m invincible',
+	               'OK! I moon the Balrog!',
+	               'Well, we know he\'s LAWFUL evil, so he should keep his word when he promised not to betray us.',
+	               'Yeah, I know it\'s dangerous, but think of the experience points.',
+	               'They can\'t see me. I\'m invisible!',
+	               'They don\'t look so tough.',
+	               'What do you mean trolls regenerate!?!',
+	               'Those noises are probably nothing.',
+	               'He shot out my eye? Okay, I tear out my other eye and throw it at him as a gesture of defiance.',
+	               'This is a push-over dungeon.',
+	               'I know an illusion when I see one.',
+	               'Is this one really able to breath fire?',
+	               'There’s no such thing as a bottomless pit. Everybody knows that.',
+	               'I spent seven hours rolling this guy up'
+	               ];
+	
+	this.quote = this.getQuote();
 }
-
-/**
- * Removes a given index from the combat
- */
-MainCtrl.prototype.removeInitiative = function(index)
-{
-	this.combat.splice(index, 1);
-};
 
 /**
  * Adds a given initiative to the combat
@@ -41,14 +74,23 @@ MainCtrl.prototype.addInitiative = function()
 {
 	// Add the new initiative to the combat
 	this.combat.push(new Initiative(this.newInitiative.name, parseInt(this.newInitiative.init), this.newInitiative.type));
+	this.counter++;
 	
 	// Reset the form fields and return the user to the first one
 	this.newInitiative.name = '';
 	this.newInitiative.init = 0;
 	this.newInitiative.type = '';
 	
-	//TODO: This has got to be easier in Angular/jQuery
+	// Focus back on the name field
 	document.getElementById('newInitiative.name').focus();
+};
+
+/**
+ * Removes a given initiative id from the combat
+ */
+MainCtrl.prototype.removeInitiative = function(index)
+{
+	this.combat.splice(index, 1);
 };
 
 /**
@@ -57,14 +99,9 @@ MainCtrl.prototype.addInitiative = function()
 MainCtrl.prototype.setMode = function(mode)
 {
 	if(mode === 'combat') {
-		// We are switching to combat mode, change from initiative to actual combat order
-//		this.combat.forEach(function(x) {
-//			// Set the order value of the initiative to the init value
-//			x.order = x.init;
-//		});
-		
+		// We are switching to combat mode, change from initiative to actual combat order		
 		// Set the sorting and mode to combat mode
-//		this.sort = '-order';
+		this.combat.sort(function(a, b) { if(a.init < b.init) { return 1; } if(a.init > b.init) { return -1; } return 0;});
 		this.sort = '';
 		this.mode = mode;
 	}
@@ -89,6 +126,72 @@ MainCtrl.prototype.previousInit = function()
 };
 
 /**
+ * Delay the current initiative
+ */
+MainCtrl.prototype.setDelay = function()
+{
+	// Grab the current initiative and set delay
+	var current = this.combat[0];
+	current.delay = true;
+	
+	// Proceed to the next initiative	
+	this.nextInit();
+};
+
+/**
+ * Ready the current initiative
+ */
+MainCtrl.prototype.setReady = function()
+{
+	// Grab the current initiative and set ready	
+	var current = this.combat[0];
+	current.ready = true;
+	
+	// Proceed to the next initiative
+	this.nextInit();
+};
+
+/**
+ * Let a given index take his delayed action
+ */
+MainCtrl.prototype.takeDelay = function(index)
+{
+	// Grab the delayed initiative and remove delayed status
+	var current = this.combat[index];
+	current.delay = false;
+	
+	// Temporarily remove the delayed initiative from the combat
+	this.combat.splice(index, 1);
+	
+	// Apply the delayed initiative to the front of the combat order
+	this.combat.unshift(current);
+};
+
+/**
+ * Let a given index take his readied action
+ */
+MainCtrl.prototype.takeReady = function(index)
+{
+	// Grab the delayed initiative and remove ready status
+	var current = this.combat[index];
+	current.ready = false;
+	
+	// Temporarily remove the readied initiative from the combat
+	this.combat.splice(index, 1);
+	
+	// Apply the readied initiative to the front of the combat order
+	this.combat.unshift(current);
+};
+
+/**
+ * Get a random quote to show at combat startup
+ */
+MainCtrl.prototype.getQuote = function()
+{
+	return this.quotes[Math.floor(Math.random() * this.quotes.length)];
+};
+
+/**
  * Initiative constructor
  * @param name
  * @param init
@@ -99,12 +202,14 @@ function Initiative(name, init, type) {
 	this.name = name;
 	this.init = init;
 	this.type = type;
-//	this.order = 0;
+	this.delay = false;
+	this.ready = false;
 }
 
+// Setup the angular app
 angular
 .module('ccCombatApp')
 .controller('MainCtrl', MainCtrl)
-.factory('Initiative', Initiative);		
+.factory('Initiative', Initiative);
 
 }());
